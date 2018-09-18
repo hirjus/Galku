85b81db1117b7376cd623a00747b9a95a0046e8a
hirjus <jussi.hirvonen@iki.fi>
2018-09-18 16:28:49 +0300

    Riviprofiilit ja ka-rivi - kuva
    Saatiin tehdyksi, vaatii viilausta ja tarkastuksia.

---
+++ mikaon.R
@@ -0,0 +1,7 @@
+# mikä tämä on - datan tutkailua 11.9.2018
+## str(apu2_df)
+## typeof(apu2_df) # what is it?
+## class(apu2_df) # what is it? (sorry)
+## storage.mode(apu2_df) # what is it? (very sorry)
+## length(apu2_df) # how long is it? What about two dimensional objects?
+## attributes(apu2_df) 
