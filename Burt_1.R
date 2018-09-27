770775ca2e97c03a59f68cd9691edac7b5d25e77
hirjus <jussi.hirvonen@iki.fi>
2018-09-27 17:56:34 +0300

    Täydentävät pisteet, kuvia
    Burt_1.R  siisti esimerkki täydentävien rivien lisäämisestä. Burt-matriisia vain kokeiltiin. Muutama kuva lisää img-hakemistoon.

--- Burt_1.R
+++ Burt_1.R
@@ -6,7 +6,37 @@ str(test1)
 maagaBurt <- mjca(test1, ps="")$Burt
 dim(maagaBurt) #83 83
 str(maagaBurt)
+maagaBurt
 maagaBurt2 <- maagaBurt[6:83,1:5]
 str(maagaBurt2)
 head(maagaBurt2)
 maagaBurt2
+# ei ihan helppoa...
+
+
+testTab1 <- table(test1$maaga, test1$V6)
+testTab1
+
+testTab_sr <- table(test1$maa, test1$V6)
+testTab_sr
+testTab1 <- rbind(testTab1,testTab_sr)
+testTab1
+
+dim(testTab1) #78 riviä, 5 saraketta, 1-72 data ja 73-78 täydentävät rivit
+
+spCAmaaga1 <- ca(testTab1[,1:5], suprow = 73:78)
+X11()
+par("cex"= 0.75, "asp" = 1)
+plot(spCAmaaga1, main = "Äiti töissä: ikäluokka ja sukupuoli maittain 2")
+
+#asymmetrinen kartta
+X11()
+par("cex"= 0.75, "asp" = 1)
+plot(spCAmaaga1, main = "Äiti töissä: ikäluokka ja sukupuoli maittain 3 (kontribuutiot)",
+                  map = "rowgreen", 
+                  contrib= c("absolute", "absolute"),
+                  mass = c(TRUE,TRUE),
+                  arrows = c(FALSE,TRUE)
+                  )
+#numeeriset tulokset
+summary(spCAmaaga1)
